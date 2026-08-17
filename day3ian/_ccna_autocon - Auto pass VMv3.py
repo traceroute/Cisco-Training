927605e907a4e8133e85f9a3810a697b5aafffc7
traceroute <spyhunter152003@gmail.com>
2026-08-17 20:03:43 +0800

    change ip

--- day3ian/_ccna_autocon - Auto pass VMv3.py
+++ day3ian/_ccna_autocon - Auto pass VMv3.py
@@ -11,7 +11,7 @@ _3Tier = "7"
 _WAN = "8"
 _NetAuto = "9"
 
-rst_ip = "10.4.4.2"
+rst_ip = "10.5.4.9"
 lab = crt.Dialog.Prompt("""
 What Lab will you be using? (Input the number of the corresponding lab.)
 
